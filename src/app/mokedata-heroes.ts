f85b96198bf1605e653220275e7d3a75333ecebb
wavesen.shen <70527987@qq.com>
2018-08-21 18:44:51 +0800

    主从组件

---
+++ src/app/mokedata-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES : Hero[]=[
+  {id : 11 ,name : '张三' },
+  {id : 12 ,name : '李四' },
+  {id : 13 ,name : '王五' },
+  {id : 14 ,name : '赵六' },
+  {id : 15 ,name : '胜七'},
+  {id : 16 ,name : 'wawia'},
+  {id : 17 ,name : 'kfhue'},
+  {id : 18 ,name : 'asdad'},
+  {id : 19 ,name : 'sdfgdh'},
+  {id : 20 ,name : 'sdgddrt'}
+];
